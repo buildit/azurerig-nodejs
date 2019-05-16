5d10d152b6bbc96a7d45abef7bf21537d0affc5a
Nicholas Galazzo <nicholasgalazzo@Galazzo.local>
2019-05-16 14:54:35 -0400

    slack webhooks changes

--- SlackNotificationFunctionApp/index.js
+++ SlackNotificationFunctionApp/index.js
@@ -1,11 +1,28 @@
+const https = require('http');
+
 module.exports = function(context, req){
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if(req.query.name || (req.body && req.body.name)){
-        context.res = {
-            //status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+        
+        const opts = {
+            hostname: "https://hooks.slack.com/",
+            post: 443,
+            path: "services/T03ALPC1R/BGDLRTZNH/YGgnmqUs7cyCXMKcwueIlBBJ",
+            method: "POST",
+            Headers: {
+                'Content-Type': 'application/json'
+            }
         };
+
+        const request = https.request(opts, (res) => {
+            console.log(`statusCode: ${res.statusCode}`);
+
+            context.res = {
+                //status: 200, /* Defaults to 200 */
+                body: "Hello " + (req.query.name || req.body.name)
+            };
+        });   
     } else {
         context.res = {
             status: 400,
