c3cc529808eff014b72720034dafe54f8c835ea9
Nicholas Galazzo <nicholasgalazzo@Galazzo.local>
2019-05-16 10:02:26 -0400

    fixed commonRG script

--- src/scripts/createCommonResourceGroup.sh
+++ src/scripts/createCommonResourceGroup.sh
@@ -1,21 +1,27 @@
 #! /bin/bash
 
 #Create Resource Group
-az group create --name $1 --location $5
+az group create --name $1 --location $2
 
 #Create Container Registry
-az acr create --resource-group $1 --name $2 --sku Basic
+az acr create --resource-group $1 --name $3 --sku Basic
 
 #Create Storage Account
 az storage account create \
-    --name $3
-    --resource-group $2 \
-    --location $5
+    --name $4
+    --resource-group $1 \
+    --location $2
 
 #Create Slack Function App
 az functionapp create \
-     --name $4 \
+     --name $5 \
      --resource-group $1 \
-     --consumption-plan-location $5 \
-     --storage-account $6 \
+     --consumption-plan-location $2 \
+     --storage-account $4 \
      --runtime node
+
+#Create Alert
+az monitor alert create \
+    --name ErrorLogAlert
+    --description "Errors in AppInsights Trace Log"
+    --condition "Whenever the traces is greater than 1 count where SecurityLevel includes Error"
