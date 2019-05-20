b16d1163c266e9f8032e301b8937038a829a32a4
Nicholas Galazzo <nicholasgalazzo@Galazzo.local>
2019-05-20 13:49:15 -0400

    test

--- RigFunctionApp/SlackHookFunction/index.js
+++ RigFunctionApp/SlackHookFunction/index.js
@@ -1,11 +1,15 @@
-const request = require('request-promise');
-module.exports = async function(context) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const request = require("request-promise");
+
+module.exports = async function (context, req) {
+    context.log("JavaScript HTTP trigger function processed a request.");
+
+    context.log("JavaScript HTTP trigger function processed a request.");
     await request.post({
-        url: 'https://hooks.slack.com/services/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',
-            method: 'POST',
+            url: "https://hooks.slack.com/services/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",
+            method: "POST",
             json: {
-                "text": 'Hello World'
-                };
+                "text": "Hello World"
+                }
             });
-}
+
+};
