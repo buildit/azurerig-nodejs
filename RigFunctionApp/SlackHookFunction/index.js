f27df765982bc2315a5b8eaab3a256394f7b1abb
Nicholas Galazzo <nicholasgalazzo@Galazzo.local>
2019-05-20 10:25:45 -0400

    updated Azure Function

--- RigFunctionApp/SlackHookFunction/index.js
+++ RigFunctionApp/SlackHookFunction/index.js
@@ -1,36 +1,38 @@
-const https = require('http');
+const https = require('request');
 
-module.exports = function(context, req){
+module.exports = async function(context, req){
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if(req.query.name || (req.body && req.body.name)){
-        
-        const opts = {
+    request.post({
+        url: 'https://hooks.slack.com/services/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',
+        method: 'POST',
+        json: {text: 'Hello World'}
+    });
+
+    const post_data =  {
+                "text": "Hello, world."
+            };
+
+    const opts = {
             hostname: "hooks.slack.com",
             post: 443,
             path: "/services/T03ALPC1R/BGDLRTZNH/YGgnmqUs7cyCXMKcwueIlBBJ",
             method: "POST",
-            body: {
-                "text": "Hello, world."
-            },
             Headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Content-Length': post_data.length
             }
         };
 
-        const request = https.request(opts, (res) => {
-            console.log(`statusCode: ${res.statusCode}`);
+    let post_req = https.request(opts);
+   
+ //   context.response = {
+        //status: 200, /* Defaults to 200 */
+//        body: "Hello " + (req.query.name || req.body.name)
+  //  }
 
-            context.res = {
-                //status: 200, /* Defaults to 200 */
-                body: "Hello " + (req.query.name || req.body.name)
-            };
-        });   
-    } else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
-    context.done();
+  post_req.write(post_data);
+  post_req.end();
+       
+  context.done();
 };
