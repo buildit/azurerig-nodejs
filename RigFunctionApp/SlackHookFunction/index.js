51ab83f36a2f08c5182a8e50dc54a0b10d20b070
Nicholas Galazzo <nicholasgalazzo@Galazzo.local>
2019-05-20 15:20:45 -0400

    test

--- RigFunctionApp/SlackHookFunction/index.js
+++ RigFunctionApp/SlackHookFunction/index.js
@@ -16,5 +16,7 @@ module.exports =function (context, req) {
             // status: 200, /* Defaults to 200 */
             body: "Hello " + (req.query.name || req.body.name)
         };
+
+        context.done();
     });
 };
