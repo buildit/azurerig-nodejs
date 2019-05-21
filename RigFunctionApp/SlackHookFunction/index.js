6a4f40fb802f07d507573a07a2683fed025a537e
Nicholas Galazzo <nicholasgalazzo@Galazzo.local>
2019-05-21 10:18:11 -0400

    test

--- RigFunctionApp/SlackHookFunction/index.js
+++ RigFunctionApp/SlackHookFunction/index.js
@@ -1,6 +1,6 @@
 var request = require("request-promise");
 
-module.exports = async function (context, req) {
+module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     context.log(process.env["SLACK_HOOK_URL"]);
 
